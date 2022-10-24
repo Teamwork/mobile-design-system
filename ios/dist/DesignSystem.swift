a0860904bc736c13f87aba07e2af3baf4eeddf6e
andreaagudo3 <andreaagudo3@users.noreply.github.com>
2022-10-24 17:13:48 +0000

    Update Tokens

--- ios/dist/DesignSystem.swift
+++ ios/dist/DesignSystem.swift
@@ -23,42 +23,37 @@ enum DesignSystem {
 
  enum LineHeights {
 
-  public static let 0 = CGFloat(44)
-  public static let 1 = CGFloat(52)
-  public static let 2 = CGFloat(64)
-  public static let 3 = CGFloat(32)
-  public static let 4 = CGFloat(36)
-  public static let 5 = CGFloat(40)
-  public static let 6 = CGFloat(20)
-  public static let 7 = CGFloat(24)
-  public static let 8 = CGFloat(28)
-  public static let 9 = CGFloat(16)
+  public static let leading01 = CGFloat(44)
+  public static let leading02 = CGFloat(52)
+  public static let leading03 = CGFloat(64)
+  public static let leading04 = CGFloat(32)
+  public static let leading05 = CGFloat(36)
+  public static let leading06 = CGFloat(40)
+  public static let leading07 = CGFloat(20)
+  public static let leading08 = CGFloat(24)
+  public static let leading09 = CGFloat(28)
+  public static let leading10 = CGFloat(16)
  }
 
  enum FontSizes {
 
-  public static let 0 = CGFloat(11)
-  public static let 1 = CGFloat(12)
-  public static let 2 = CGFloat(14)
-  public static let 3 = CGFloat(16)
-  public static let 4 = CGFloat(22)
-  public static let 5 = CGFloat(24)
-  public static let 6 = CGFloat(28)
-  public static let 7 = CGFloat(32)
-  public static let 8 = CGFloat(36)
-  public static let 9 = CGFloat(45)
-  public static let 10 = CGFloat(57)
+  public static let fontSize01 = CGFloat(11)
+  public static let fontSize02 = CGFloat(12)
+  public static let fontSize03 = CGFloat(14)
+  public static let fontSize04 = CGFloat(16)
+  public static let fontSize05 = CGFloat(22)
+  public static let fontSize06 = CGFloat(24)
+  public static let fontSize07 = CGFloat(28)
+  public static let fontSize08 = CGFloat(32)
+  public static let fontSize09 = CGFloat(36)
+  public static let fontSize10 = CGFloat(45)
+  public static let fontSize11 = CGFloat(57)
  }
 
  enum LetterSpacing {
 
-  public static let 5 = CGFloat(0.4)
-  public static let 6 = CGFloat(0.25)
-  public static let 7 = CGFloat(0.5)
- }
-
- enum ParagraphSpacing {
-
-  public static let 0 = CGFloat(0)
+  public static let tracking06 = CGFloat(0.4)
+  public static let tracking07 = CGFloat(0.25)
+  public static let tracking08 = CGFloat(0.5)
  }
 }
