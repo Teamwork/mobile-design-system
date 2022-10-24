0e83bbecbb5c9f93abc55288414bc99c063c1080
andreaagudo3 <andreaagudo3@users.noreply.github.com>
2022-10-24 14:31:33 +0000

    Update Tokens

--- ios/dist/DesignSystem.swift
+++ ios/dist/DesignSystem.swift
@@ -12,32 +12,12 @@ enum DesignSystem {
 
  enum BorderRadius {
 
-  public static let 0 = CGFloat(0)
+  public static let none = CGFloat(0)
   public static let xs = CGFloat(4)
   public static let sm = CGFloat(8)
   public static let md = CGFloat(12)
   public static let lg = CGFloat(16)
   public static let xl = CGFloat(34)
   public static let full = CGFloat(100)
- }
-
- enum Spacing {
-
-  public static let 1 = CGFloat(2)
-  public static let 2 = CGFloat(4)
-  public static let 3 = CGFloat(8)
-  public static let 4 = CGFloat(12)
-  public static let 5 = CGFloat(16)
-  public static let 6 = CGFloat(24)
-  public static let 7 = CGFloat(32)
-  public static let 8 = CGFloat(40)
-  public static let 9 = CGFloat(44)
-  public static let 10 = CGFloat(48)
-  public static let 11 = CGFloat(56)
-  public static let 12 = CGFloat(64)
-  public static let 13 = CGFloat(72)
-  public static let 14 = CGFloat(80)
-  public static let 15 = CGFloat(112)
-  public static let 16 = CGFloat(128)
  }
 }
