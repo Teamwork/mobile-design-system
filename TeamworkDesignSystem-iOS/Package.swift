c664b9b18028c5ce583f787b1a0e6dba0bbc5f06
Andrea Agudo <andrea.agudo@teamwork.com>
2022-10-25 14:32:18 +0200

    updated package

--- TeamworkDesignSystem-iOS/Package.swift
+++ TeamworkDesignSystem-iOS/Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TeamworkDesignSystem-iOS",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/TeamworkDesignSystem-iOS"),
         .testTarget(
             name: "TeamworkDesignSystem-iOSTests",
             dependencies: ["TeamworkDesignSystem-iOS"]),
