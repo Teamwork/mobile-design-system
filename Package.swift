3a2238b1c649b2e130cc6438f3f2a44804383e18
Andrea Agudo <andrea.agudo@teamwork.com>
2022-10-25 14:53:07 +0200

    package name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TeamworkDesignSystem-iOS",
+            name: "TeamworkDesignSystemiOS",
             targets: ["TeamworkDesignSystem-iOS"]),
     ],
     dependencies: [
