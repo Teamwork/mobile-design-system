36c407e41666c13c9414f689391de3decbdb5cc0
Andrea Agudo <andrea.agudo@teamwork.com>
2022-11-03 14:12:57 +0100

    deleted system colors

--- ios/Sources/InApp/InAppUIColor.swift
+++ ios/Sources/InApp/InAppUIColor.swift
@@ -22,23 +22,6 @@ public extension UIColor {
         public static let interactionPrimary = UIColor(named: "interactionPrimary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
         public static let interactionPrimaryHighlight = UIColor(named: "interactionPrimaryHighlight", in: bundle, compatibleWith: nil).unsafelyUnwrapped
         public static let interactionSecondary = UIColor(named: "interactionSecondary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        
-        // MARK: - Fill
-
-        public static let secondarySystemFill = UIColor.secondarySystemFill
-        public static let tertiarySystemFill = UIColor.tertiarySystemFill
-        public static let quaternarySystemFill = UIColor.quaternarySystemFill
-        
-        // MARK: - System Background
-        
-        public static let systemBackground = UIColor.systemBackground
-        public static let secondarySystemBackground = UIColor.secondarySystemBackground
-        
-        // MARK: - Label
-        
-        public static let label = UIColor.label
-        public static let secondaryLabel = UIColor.secondaryLabel
-        public static let tertiaryLabel = UIColor.tertiaryLabel
 
         // MARK: - Neutral
         public static let neutralBlack = UIColor(named: "ui-neutral-black", in: bundle, compatibleWith: nil).unsafelyUnwrapped
