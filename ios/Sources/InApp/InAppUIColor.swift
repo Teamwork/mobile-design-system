26bea82f75d5f6c70ceba1657b76d7515ccfdade
Andrea Agudo <andrea.agudo@teamwork.com>
2022-11-03 14:18:41 +0100

    deleted teamwork enum

--- ios/Sources/InApp/InAppUIColor.swift
+++ ios/Sources/InApp/InAppUIColor.swift
@@ -3,31 +3,28 @@ import UIKit
 
 public extension UIColor {
     
-    enum teamwork {
+    struct DynamicUIColor: Hashable {
         
-        public struct DynamicUIColor: Hashable {
-            
-            public let base: UIColor
-            public let child10: UIColor
-            public let child20: UIColor
-            public let child40: UIColor
-            public let child50: UIColor
-        }
-        
-        public static let separator = UIColor.separator
-
-        // MARK: - Feedback
-        
-        public static let errorPrimary = UIColor(named: "errorPrimary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let interactionPrimary = UIColor(named: "interactionPrimary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let interactionPrimaryHighlight = UIColor(named: "interactionPrimaryHighlight", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let interactionSecondary = UIColor(named: "interactionSecondary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-
-        // MARK: - Neutral
-        public static let neutralBlack = UIColor(named: "ui-neutral-black", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let neutralWhite = UIColor(named: "ui-neutral-white", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let neutralLight = UIColor(named: "default-neutral-light", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let neutralDark = UIColor(named: "default-neutral-dark", in: bundle, compatibleWith: nil).unsafelyUnwrapped
-        public static let neutralShadow = UIColor(named: "default-neutral-shadow", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+        public let base: UIColor
+        public let child10: UIColor
+        public let child20: UIColor
+        public let child40: UIColor
+        public let child50: UIColor
     }
+    
+    static let separator = UIColor.separator
+    
+    // MARK: - Feedback
+    
+    static let errorPrimary = UIColor(named: "errorPrimary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let interactionPrimary = UIColor(named: "interactionPrimary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let interactionPrimaryHighlight = UIColor(named: "interactionPrimaryHighlight", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let interactionSecondary = UIColor(named: "interactionSecondary", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    
+    // MARK: - Neutral
+    static let neutralBlack = UIColor(named: "ui-neutral-black", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let neutralWhite = UIColor(named: "ui-neutral-white", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let neutralLight = UIColor(named: "default-neutral-light", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let neutralDark = UIColor(named: "default-neutral-dark", in: bundle, compatibleWith: nil).unsafelyUnwrapped
+    static let neutralShadow = UIColor(named: "default-neutral-shadow", in: bundle, compatibleWith: nil).unsafelyUnwrapped
 }
