30461fa68653199b23fca393be914138c448060d
Aidan Grabe <aidan@teamwork.com>
2022-12-20 16:30:14 +0000

    Android: Fix fontFamily in XML typography

--- formats/androidTypography.js
+++ formats/androidTypography.js
@@ -17,11 +17,26 @@ function getTextStyleForToken(token) {
     ${Object.keys(token.value).map(key => `${key} => ${token.value[key]}`).join("\n\t")}
   -->
   <style name="${token.name}">
-    <item name="android:fontFamily">${token.value.fontFamily}</item>
+    ${getFontFamilyTag(token)}
     <item name="android:textSize">${token.value.fontSize}sp</item>
     <item name="android:letterSpacing">${calculateLetterSpacing(token.value.fontSize, token.value.letterSpacing)}</item>
     ${getLineHeightTag(token)}
   </style>`;
+}
+
+function getFontFamilyTag(token) {
+  const fontFamilyValue = getFontFamilyValue(token.value.fontFamily, token.value.fontWeight);
+  if (fontFamilyValue == null) {
+    return "<!-- no fontFamily specified as we want to use the default Roboto -->"; // this is the default font, it's included in the system
+  }
+  return `<item name="android:fontFamily">${fontFamilyValue}</item>`
+}
+
+function getFontFamilyValue(fontFamily, fontWeight) {
+  if (fontFamily == "Roboto") {
+    return null; // this is the default font, it's included in the system
+  }
+  return `@font/${fontFamily.toLowerCase()}_${fontWeight.toLowerCase()}`;
 }
 
 function calculateLetterSpacing(fontSize, letterSpacing) {
