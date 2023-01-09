d0244c94b4e69e1e4f8d99ae17c112c3ba16fa9a
Aidan Grabe <aidan@teamwork.com>
2023-01-09 14:34:28 +0000

    TeamworkTheme: Add `system` aliases

--- android/styledictionary/teamwork-design-system/src/main/java/com/teamwork/design/TeamworkTheme.kt
+++ android/styledictionary/teamwork-design-system/src/main/java/com/teamwork/design/TeamworkTheme.kt
@@ -1,21 +1,40 @@
+@file:Suppress("unused")
+
 package com.teamwork.design
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.compositionLocalOf
+import com.teamwork.design.TeamworkTheme.shapes
 import com.teamwork.design.generated.*
 import com.teamwork.design.m3.AppTheme
 
+// defaults to showing the Light colours
+private val LocalTeamworkColors = compositionLocalOf { LightColors }
+
 /**
  * Provides Composition local references to the colours, fonts etc.
+ * For convenience there are extension properties [TeamworkColors.system] and [TeamworkTypography.system] to allow easy access to the
+ * tokens defined by [MaterialTheme]. [shapes] also is an alias to [MaterialTheme.shapes] since we don't define any of our own shape
+ * tokens. This way we should be able to use [TeamworkTheme] for all tokens.
  */
+@Suppress("MemberVisibilityCanBePrivate")
 object TeamworkTheme {
 
     val color
         @Composable
+        @ReadOnlyComposable
         get() = LocalTeamworkColors.current
+
+    val shapes: Shapes
+        @Composable
+        @ReadOnlyComposable
+        get() = MaterialTheme.shapes
 
     val typography = TeamworkTypography
 
@@ -30,8 +49,19 @@ object TeamworkTheme {
 }
 
 
-// defaults to showing the Light colours
-private val LocalTeamworkColors = compositionLocalOf { LightColors }
+//region extension functions to alias the MaterialTheme into our TeamworkTheme
+
+val TeamworkColors.system: ColorScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.colorScheme
+
+val TeamworkTypography.system: androidx.compose.material3.Typography
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.typography
+
+//endregion
 
 
 /**
@@ -39,6 +69,7 @@ private val LocalTeamworkColors = compositionLocalOf { LightColors }
  */
 @Composable
 fun TeamworkTheme(content: @Composable () -> Unit) {
+    MaterialTheme.colorScheme.surface
     AppTheme {
         val teamworkColors = if (isSystemInDarkTheme()) {
             DarkColors
